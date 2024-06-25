5f310abea96adee0760a07e91ee3ff29e8efa9f9
AlbanLesellier <32831143+AlbanLesellier@users.noreply.github.com>
2024-06-25 13:36:11 +0000

    Pending changes exported from your codespace

---
+++ statLaw/src/Command/SynchroFilesOrganCommand.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:synchro-files-organ',
+    description: 'Add a short description for your command',
+)]
+class SynchroFilesOrganCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+        $pathToImport = "/workspaces/SymfonyLearningStat/statLaw/data/toImport/organe";
+        $pathImported = "/workspaces/SymfonyLearningStat/statLaw/data/imported/organe";
+        if(!is_dir($pathToImport)) {
+            $io->error('To import directory doesn\'t exist');
+            return Command::FAILURE;
+        }
+        if(!is_dir($pathImported)) {
+            $io->error('Imprted directory doesn\'t exist');
+            return Command::FAILURE;
+        }
+
+        $folder = opendir($pathToImport);
+        $files = scandir($pathToImport);
+
+        foreach ($io->progressIterate($files) as $file) {
+      
+          
+            if(is_file($pathToImport."/".$file) && !is_file($pathImported."/".$file)) {
+                $output->writeln($file);
+                $content = file_get_contents($pathToImport."/".$file);
+                $data = json_decode($content);
+                file_put_contents($pathImported."/".$file.".txt",print_r($data,true));
+                //$output->writeln(print_r($data,true));
+                return Command::SUCCESS;
+            } else {
+                @unlink($pathToImport."/".$file);
+            }
+        
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
--- statLaw/src/Controller/ActorController.php
+++ statLaw/src/Controller/ActorController.php
@@ -5,14 +5,27 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ActorRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Psr\Log\LoggerInterface;
 
 class ActorController extends AbstractController
 {
     #[Route('/actor', name: 'app_actor')]
-    public function index(): Response
+    public function index(  
+        ActorRepository $actorRepository,
+        Request $request,
+        ManagerRegistry $doctrine,
+        LoggerInterface $logger
+    ): Response
     {
+        
+        $actors = $actorRepository->findAll();
         return $this->render('actor/index.html.twig', [
             'controller_name' => 'ActorController',
+            'actors' => $actors
         ]);
     }
 }
